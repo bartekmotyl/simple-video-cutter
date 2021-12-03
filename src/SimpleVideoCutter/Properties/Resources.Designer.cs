2d70f2a84b489820f8b0972e108803c9017b7160
bartekmotyl <29923773+bartekmotyl@users.noreply.github.com>
2021-12-03 15:20:05 +0100

    Upgade libs

--- src/SimpleVideoCutter/Properties/Resources.Designer.cs
+++ src/SimpleVideoCutter/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SimpleVideoCutter.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -253,6 +253,26 @@ namespace SimpleVideoCutter.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap streamline_icon_interface_edit_view_32x32 {
+            get {
+                object obj = ResourceManager.GetObject("streamline_icon_interface_edit_view_32x32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap streamline_icon_interface_edit_view_off_32x32 {
+            get {
+                object obj = ResourceManager.GetObject("streamline_icon_interface_edit_view_off_32x32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap streamline_icon_laptop_download_32x32 {
             get {
                 object obj = ResourceManager.GetObject("streamline-icon-laptop-download@32x32", resourceCulture);
@@ -286,6 +306,16 @@ namespace SimpleVideoCutter.Properties {
         internal static System.Drawing.Bitmap streamline_icon_navigation_right_to_32x32 {
             get {
                 object obj = ResourceManager.GetObject("streamline-icon-navigation-right-to@32x32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap streamline_icon_seo_zoom_32x32 {
+            get {
+                object obj = ResourceManager.GetObject("streamline_icon_seo_zoom_32x32", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
